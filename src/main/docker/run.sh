cc4434e46fbcfa6a11f990f7cdcd16a5d51178ad
YimengL <yimengcsu@gmail.com>
2020-02-23 17:42:15 -0600

    add boilerplate code

---
+++ src/main/docker/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo "********************************************************"
+echo "Waiting fot the configuration server to  start on port $CONFIGURATION_PORT"
+echo "********************************************************"
+while ! `nc -z configserver  $CONFIGSERVER_PORT`; do sleep 3; done
+echo ">>>>>>>>>>>> Configuration Server has started"
+
+echo "********************************************************"
+echo "Waiting for the database server to start on port $DATABASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+
+echo "********************************************************"
+echo "Starting License Server with Configuration Service: $CONFIGSERVER_URI"
+echo "********************************************************"
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/licensing-service-0.0.1-SNAPSHOT.jar
