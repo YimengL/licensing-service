f8af12ff427b89ef7f99f1befd69003e0d156f29
YimengL <yimengcsu@gmail.com>
2020-02-23 20:46:47 -0600

    fix typos

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 echo "********************************************************"
-echo "Waiting fot the configuration server to  start on port $CONFIGURATION_PORT"
+echo "Waiting for the configuration server to start on port: $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver  $CONFIGSERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASERVER_PORT"
+echo "Waiting for the database server to start on port: $DATABASESERVER_PORT"
 echo "********************************************************"
 while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 
 echo "********************************************************"
-echo "Starting License Server with Configuration Service: $CONFIGSERVER_URI"
+echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/licensing-service-0.0.1-SNAPSHOT.jar
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
