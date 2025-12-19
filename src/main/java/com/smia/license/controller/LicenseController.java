757affccd235fbf78ed7bb5745683c5e29d7f7fe
YimengL <yimengcsu@gmail.com>
2025-12-19 20:50:24 +0100

    smia - ch03 - internationalization

--- src/main/java/com/smia/license/controller/LicenseController.java
+++ src/main/java/com/smia/license/controller/LicenseController.java
@@ -4,14 +4,9 @@ import com.smia.license.model.License;
 import com.smia.license.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController // REST service and handle JSON Serialization
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -36,8 +31,9 @@ public class LicenseController {
 
     @PostMapping
     public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
-                                                @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+                                                @RequestBody License request,
+                                                @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
     }
 
     @DeleteMapping(value = "/{licenseId}")
